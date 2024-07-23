2cb179b6eede44e78d0323521d63eefac57992fa
MikeGhoul <mike.ghoul5@gmail.com>
2024-07-23 18:43:05 -0400

    additional solution

--- SQL/03. Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
+++ SQL/03. Basic Aggregate Functions/1211. Queries Quality and Percentage.sql
@@ -78,6 +78,16 @@ FROM Queries
 WHERE query_name IS NOT NULL
 GROUP BY 1
 
+-- Approach manualy calculating the average:
+
+SELECT
+query_name
+, ROUND(SUM(rating / position) / COUNT(rating), 2) AS quality
+, ROUND((SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*)) * 100, 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY 1
+
 
 
 
